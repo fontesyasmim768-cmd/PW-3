fd2bb5178c7cf49f3dd92b1618b8fddf11a829e9
fontesyasmim768-cmd <fontesyasmim768@gmail.com>
2026-05-21 20:48:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("formCadastro");
+const mensagem = document.getElementById("mensagem");
+
+form.addEventListener("submit", async function(event) {
+
+    event.preventDefault();
+
+    const nome = document.getElementById("nome"),value;
+    const email = document.getElementById("email"),value;
+    const senha = document.getElementById("senha"),value;
+
+    //validar email
+
+
+    
+})
